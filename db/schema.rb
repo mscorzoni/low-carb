f86c7f68e82339ee2d9a05500b842f63ef589710
Marcos Scorzoni <mlessa@gmail.com>
2018-09-24 12:14:15 +0100

    add seed file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_24_105922) do
+ActiveRecord::Schema.define(version: 2018_09_24_110723) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,4 +24,13 @@ ActiveRecord::Schema.define(version: 2018_09_24_105922) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "ingredients", force: :cascade do |t|
+    t.bigint "drink_id"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["drink_id"], name: "index_ingredients_on_drink_id"
+  end
+
+  add_foreign_key "ingredients", "drinks"
 end
